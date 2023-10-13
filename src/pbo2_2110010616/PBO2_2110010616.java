903c53716f28c6df11236bfe115253763f3e29b5
DreamNinjas <mziyadrgt@gmail.com>
2023-10-13 11:11:34 +0800

    Initial commit

---
+++ src/pbo2_2110010616/PBO2_2110010616.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pbo2_2110010616;
+import database.koneksi;
+import java.sql.SQLException;
+import forms.*;
+/**
+ *
+ * @author LENOVO
+ */
+public class PBO2_2110010616 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new koneksi();
+        new login().setVisible(true);
+        
+    }
+    
+}
